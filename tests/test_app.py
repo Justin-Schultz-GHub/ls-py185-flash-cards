26dee0e9ebf8abd54d7001adf28562ec4f4d6479
Justin-Schultz-GHub <SchultzJustin@ymail.com>
2026-03-07 01:17:34 +0000

    Refactor tests to use DatabasePersistence and app context properly, update .gitignore

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,35 +1,32 @@
 import unittest
-import os
-import shutil
-import yaml
-from app import app, get_data_dir, get_deck_path, get_yaml_path, generate_next_folder_name, deck_exists
+from app import app
+from flashcards.database_persistence import DatabasePersistence
+from flashcards.session_persistence import SessionPersistence
+import logging
+from flask import g
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
 
 class AppTest(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
+        app.config['DATABASE'] = 'flashcards_test'
         self.client = app.test_client()
-        self.data_path = os.path.join(os.path.dirname(__file__), 'data')
-        os.makedirs(self.data_path, exist_ok=True)
+        self.db = DatabasePersistence(dbname=app.config['DATABASE'])
 
-        for i in range(1, 4):
-            self.create_deck(f'Test Deck {i}', self.data_path)
+        self.deck_ids = [self.create_deck(f'Test Deck {i}') for i in range(1, 4)]
 
-    def create_deck(self, deckname, data_path):
-        deck_folder = generate_next_folder_name()
-        os.makedirs(get_deck_path(deck_folder), exist_ok=True)
-        yaml_path = get_yaml_path(deck_folder)
+        self.card_ids = [self.db.create_card(f'Card Front {i}', f'Card Back {i}', self.deck_ids[1]) for i in range(1, 4)]
 
-        deck_data = {
-            'name': deckname,
-            'cards': [
-                        {'front': 'Card 1 Front', 'back': 'Card 1 Back', 'id': '1'},
-                        {'front': 'Card 2 Front', 'back': 'Card 2 Back', 'id': '2'},
-                        {'front': 'Card 3 Front', 'back': 'Card 3 Back', 'id': '3'},
-                    ]
-        }
+    def create_deck(self, deckname):
+        return self.db.create_deck(deckname)
 
-        with open(yaml_path, 'w', encoding='utf-8') as file:
-            yaml.dump(deck_data, file, allow_unicode=True, default_flow_style=False, sort_keys=False)
+    def with_app_context(self, func, *args, **kwargs):
+        with app.app_context():
+            g.storage = self.db
+            return func(*args, **kwargs)
 
     def test_index(self):
         response = self.client.get('/')
@@ -73,18 +70,22 @@ class AppTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         data = response.get_data(as_text=True)
         self.assertIn('Deck name cannot be empty.', data)
-        self.assertFalse(deck_exists(f'{self.data_path}/deck4'))
+        deck = self.db.get_deck(4)
+        self.assertEqual(deck, [])
 
     def test_display_rename_deck_page(self):
-        response = self.client.get('/decks/deck1/rename')
-        self.assertEqual(response.status_code, 200)
-        data = response.get_data(as_text=True)
-        self.assertIn('Renaming Test Deck 1', data)
-        self.assertIn('Enter new deck name:', data)
+        with app.app_context():
+            g.storage = self.db
+            response = self.client.get(f'/decks/{self.deck_ids[0]}/rename')
+            self.assertEqual(response.status_code, 200)
+            data = response.get_data(as_text=True)
+            self.assertIn(f'Renaming Test Deck 1', data)
+            self.assertIn('Enter new deck name:', data)
 
     def test_rename_deck(self):
         deckname = 'Test Deck 45'
-        response = self.client.post('/decks/deck1',
+        deck_id = self.deck_ids[0]
+        response = self.client.post(f'/decks/{deck_id}/rename',
                                     data={
                                         'deckname': deckname,
                                     },
@@ -97,13 +98,9 @@ class AppTest(unittest.TestCase):
 
     def test_rename_deck_empty_name(self):
         deckname = ''
-        deck_folder = 'deck3'
-        yaml_path = get_yaml_path(deck_folder)
-
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            original_deck_data = yaml.safe_load(file)
-
-        response = self.client.post(f'/decks/{deck_folder}',
+        deck_id = self.deck_ids[0]
+        original_deck_data = self.db.get_deck(deck_id)[0]
+        response = self.client.post(f'/decks/{deck_id}/rename',
                                     data={
                                         'deckname': deckname,
                                     },
@@ -113,53 +110,52 @@ class AppTest(unittest.TestCase):
         self.assertIn(f"Renaming {original_deck_data['name']}:", data)
         self.assertIn('Deck name cannot be empty.', data)
 
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            new_deck_data = yaml.safe_load(file)
+        new_deck_data = self.db.get_deck(deck_id)[0]
 
         self.assertEqual(original_deck_data, new_deck_data)
 
     def test_delete_deck(self):
-        deck = 'deck3'
-        response = self.client.post(f'/decks/{deck}/delete',
+        deck_id = self.deck_ids[0]
+        response = self.client.post(f'/decks/{deck_id}/delete',
                                     follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         data = response.get_data(as_text=True)
         self.assertIn('Deck successfully deleted.', data)
-        self.assertNotIn('Test Deck 3', data)
+        self.assertNotIn('Test Deck 1', data)
 
     def test_delete_non_existent_deck(self):
-        deck_folder = 'deck4'
-        response = self.client.post(f'/decks/{deck_folder}/delete',
+        deck_id = 10
+        response = self.client.post(f'/decks/{deck_id}/delete',
                                     follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         data = response.get_data(as_text=True)
-        self.assertIn('Failed to delete deck.', data)
+        self.assertIn('Deck not found.', data)
         self.assertIn('Test Deck 1', data)
         self.assertIn('Test Deck 2', data)
         self.assertIn('Test Deck 3', data)
 
     def test_delete_deck_get_request(self):
-        deck_folder = 'deck3'
-        response = self.client.get(f'/decks/{deck_folder}/delete',
+        deck_id = self.deck_ids[0]
+        response = self.client.get(f'/decks/{deck_id}/delete',
                                     follow_redirects=True)
         self.assertEqual(response.status_code, 405)
-        folders = os.listdir(get_data_dir())
-        self.assertIn(deck_folder, folders)
+        self.assertTrue(self.db.get_deck(deck_id))
 
     def test_display_new_card_page(self):
-        deck_folder = 'deck3'
-        response = self.client.get(f'/decks/{deck_folder}/new_card')
+        deck_id = self.deck_ids[0]
+        response = self.client.get(f'/decks/{deck_id}/new_card')
         self.assertEqual(response.status_code, 200)
         data = response.get_data(as_text=True)
         self.assertIn('Create a New Flashcard', data)
 
     def test_create_card(self):
-        deck_folder = 'deck3'
-        response = self.client.post(f'/decks/{deck_folder}/new_card/create',
-                                    data={
-                                        'front': 'Test1',
-                                        'back': 'Test2'
-                                    },
+        deck_id = self.deck_ids[0]
+        card_data = {
+                'front': 'Test1',
+                'back': 'Test2',
+                }
+        response = self.client.post(f'/decks/{deck_id}/new_card/create',
+                                    data=card_data,
                                     follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         data = response.get_data(as_text=True)
@@ -167,28 +163,22 @@ class AppTest(unittest.TestCase):
         self.assertIn('Test1', data)
         self.assertIn('Test2', data)
 
-        yaml_path = get_yaml_path(deck_folder)
+        card = self.db.get_cards(deck_id)[0]
 
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            deck_data = yaml.safe_load(file)
-
-        self.assertIn('id', deck_data['cards'][-1])
+        self.assertEqual(card['front'], card_data['front'])
+        self.assertEqual(card['back'], card_data['back'])
 
     def test_create_card_empty_inputs(self):
-        deck_folder = 'deck3'
-        yaml_path = get_yaml_path(deck_folder)
-
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            original_deck_data = yaml.safe_load(file)
-
+        deck_id = self.deck_ids[0]
         data_sets = [
                         {'front': 'Test1', 'back': ''},
                         {'front': '', 'back': 'Test2'},
                         {'front': '', 'back': ''}
                     ]
+        original_deck_data = self.db.get_deck(deck_id)
 
         for data_set in data_sets:
-            response = self.client.post(f'/decks/{deck_folder}/new_card/create',
+            response = self.client.post(f'/decks/{deck_id}/new_card/create',
                                         data=data_set,
                                         follow_redirects=True)
             self.assertEqual(response.status_code, 200)
@@ -196,22 +186,26 @@ class AppTest(unittest.TestCase):
             self.assertIn('Create a New Flashcard:', data)
             self.assertIn('Flashcards must have a front and back.', data)
 
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            new_deck_data = yaml.safe_load(file)
+        new_deck_data = self.db.get_deck(deck_id)
 
         self.assertEqual(original_deck_data, new_deck_data)
 
     def test_delete_card(self):
-        deck_folder = 'deck2'
-        card_id = '2'
-        self.client.post(f'/decks/{deck_folder}/{card_id}/delete')
-        yaml_path = get_yaml_path(deck_folder)
+        deck_id = self.deck_ids[1]
+        card_id = self.card_ids[1]
+        old_cards = self.db.get_cards(deck_id)
 
-        with open(yaml_path, 'r', encoding='utf-8') as file:
-            deck_data = yaml.safe_load(file)
+        self.client.post(f'/decks/{deck_id}/{card_id}/delete')
 
-        for card in deck_data['cards']:
-            self.assertNotEqual(card_id, card['id'])
+        new_cards = self.db.get_cards(deck_id)
+
+        self.assertNotEqual(old_cards, new_cards)
+
+        for card in new_cards:
+            self.assertNotEqual(card['id'], card_id)
 
     def tearDown(self):
-        shutil.rmtree(self.data_path, ignore_errors=True)
+        with self.db._database_connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('DELETE FROM cards;')
+                cursor.execute('DELETE FROM decks;')
